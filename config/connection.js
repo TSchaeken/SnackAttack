a9f7adf1c2f0606aeee5df2d6dd5253fd4077e14
Tim Schaeken <tschaeken@gmail.com>
2018-05-25 12:19:08 -0700

    test

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql');
-let connection;
+var connection;
 
 if (process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
+  connection = mysql.createConnection({
   port: 3306,
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'burgers_db'
 });
-}
+};
 connection.connect(err => {
   if (err) {
     console.error(`error connecting my dude, check: ${err.stack}`);
